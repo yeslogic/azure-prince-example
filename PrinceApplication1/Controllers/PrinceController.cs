e231a051affc6783e5066be6fa7984dc014ac010
Wesley Moore <wes@wezm.net>
2020-09-23 10:30:37 +1000

    Don't cache ContentRoot and name private members consistently

--- PrinceApplication1/Controllers/PrinceController.cs
+++ PrinceApplication1/Controllers/PrinceController.cs
@@ -10,25 +10,22 @@ namespace PrinceApplication1.Controllers
     public class PrinceController : ControllerBase
     {
         private const string ExampleHtml = "<html><head><style type='text/css'>body { font-family: 'Inter' }</style></head><body>Hello from Prince</body></html>";
-        private readonly ILogger<PrinceController> _logger;
         private readonly IWebHostEnvironment _env;
-        private readonly string princePath;
-        private readonly PrinceLogger princeLogger;
-        private readonly string fontsCssPath;
+        private readonly PrinceLogger _princeLogger;
 
         public PrinceController(ILogger<PrinceController> logger, IWebHostEnvironment env)
         {
-            _logger = logger;
             _env = env;
-            princePath = Path.Combine(_env.ContentRootPath, @"prince\bin\prince.exe");
-            princeLogger = new PrinceLogger(_logger);
-            fontsCssPath = Path.Combine(_env.ContentRootPath, @"fonts\inter.css");
+            _princeLogger = new PrinceLogger(logger);
+
         }
 
         [HttpGet]
         public FileStreamResult Get()
         {
-            Prince prn = new Prince(princePath, princeLogger);
+            string princePath = Path.Combine(_env.ContentRootPath, @"prince\bin\prince.exe");
+            string fontsCssPath = Path.Combine(_env.ContentRootPath, @"fonts\inter.css");
+            Prince prn = new Prince(princePath, _princeLogger);
             prn.AddStyleSheet(fontsCssPath);
             Stream pdfOutput = new MemoryStream(10000);
             prn.ConvertString(ExampleHtml, pdfOutput);
